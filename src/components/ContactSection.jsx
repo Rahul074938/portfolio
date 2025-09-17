89e90d1f9c5e1e0e2242edb088eaffe5000cb5e6
Dandgey Rahul <165761165+Rahul074938@users.noreply.github.com>
2025-09-17 23:13:41 +0530

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -73,7 +73,7 @@ export const ContactSection = () => {
                     href="tel:+91 6303891614"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    +1 (123) 456-7890
+                    +91 6303891614
                   </a>
                 </div>
               </div>
@@ -84,7 +84,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium"> Location</h4>
                   <a className="text-muted-foreground hover:text-primary transition-colors">
-                    Vancouver, BC, Canada
+                    Hyderabad, Telangana, India
                   </a>
                 </div>
               </div>
@@ -93,11 +93,8 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/dandgey-rahul/" target="_blank">
                   <Linkedin />
-                </a>
-                <a href="#" target="_blank">
-                  <Twitter />
                 </a>
                 <a href="#" target="_blank">
                   <Instagram />
@@ -130,7 +127,7 @@ export const ContactSection = () => {
                   name="name"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="Pedro Machado..."
+                  placeholder="Dandgey Rahul..."
                 />
               </div>
 
@@ -148,7 +145,7 @@ export const ContactSection = () => {
                   name="email"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="john@gmail.com"
+                  placeholder="example@gmail.com"
                 />
               </div>
 
